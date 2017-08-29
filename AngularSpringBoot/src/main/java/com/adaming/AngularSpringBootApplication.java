c8d2e4768e6974384942a6f58927818c69554f42
mfessi <mfessi@DESKTOP-4G013UA.neylingservices.tn>
2017-08-29 12:57:48 +0100

    first commit

---
+++ AngularSpringBoot/src/main/java/com/adaming/AngularSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootApplication.class, args);
+	}
+}
